d57c6ed790548a5971fa58cc5a35a2419580ca57
Lucia Quirke <luciaq@canva.com>
2022-11-27 22:55:17 +1100

    add ex7 solutions p2

--- ex7-octave/computeCentroids.m
+++ ex7-octave/computeCentroids.m
@@ -10,31 +10,12 @@ function centroids = computeCentroids(X, idx, K)
 %   assigned to it.
 %
 
-% Useful variables
 [m n] = size(X);
 
-% You need to return the following variables correctly.
 centroids = zeros(K, n);
 
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Go over every centroid and compute mean of all points that
-%               belong to it. Concretely, the row vector centroids(i, :)
-%               should contain the mean of the data points assigned to
-%               centroid i.
-%
-% Note: You can use a for-loop over the centroids to compute this.
-%
-
-
-
-
-
-
-
-
-% =============================================================
-
-
+for i = 1 : size(centroids, 1)
+    centroidIndices = find(idx == i);
+    centroids(i, :) = mean(X(centroidIndices, :));
 end
 
