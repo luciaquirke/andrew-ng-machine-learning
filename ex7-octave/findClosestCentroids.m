589ee2642041bfef87b84cb209247e72bc041a25
Lucia Quirke <luciaq@canva.com>
2022-11-27 22:48:52 +1100

    add ex7 solutions p1

--- ex7-octave/findClosestCentroids.m
+++ ex7-octave/findClosestCentroids.m
@@ -11,23 +11,13 @@ K = size(centroids, 1);
 % You need to return the following variables correctly.
 idx = zeros(size(X,1), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Go over every example, find its closest centroid, and store
-%               the index inside idx at the appropriate location.
-%               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
-%               range 1..K
-%
-% Note: You can use a for-loop over the examples to compute this.
-%
-
-
-
-
-
-
-
-% =============================================================
+for i = 1 : size(X, 1)
+    for j = 1 : size(centroids, 1)
+        distances(j) = sum((X(i, :) - centroids(j, :)).^2);
+        [min_val, min_index] = min(distances);
+        idx(i) = min_index;
+    end
+end
 
 end
 
